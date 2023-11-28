19f494bf14e70756a5b05d714c03ec6ec043ba21
Vladimir <limtis2109@gmail.com>
2023-11-28 14:35:23 +0100

    GDC Strings Optimized

--- Strings/GCDStrings/GCDStrings.cs
+++ Strings/GCDStrings/GCDStrings.cs
@@ -2,43 +2,49 @@ namespace LeetCodeChallenge;
 
 public class GCDStrings {
     public static string FindGDCString(string x, string y) {
-        if (x.Length == y.Length && x != y) {
-            return string.Empty;
-        }
-
-        // Make X a smaller string
-        if (x.Length < y.Length) {
-            (x, y) = (y, x);
-        }
-
-        // Start from the biggest
-        for (int i = x.Length; i > 0; i--) {
-            // Ignore non-common divisors
-            if (x.Length % i != 0 || y.Length % i != 0) {
-                continue;
-            }
-
-            // Get substring of X
-            string sub = x[..i];
-
-            // Check whether it is repeating in X and Y
-            if (IsRepeatedSubstring(sub, x) && IsRepeatedSubstring(sub, y)) {
-                return sub;
-            }
-        }
-
-        return string.Empty;
+        return x + y == y + x ? y[..GCD(x.Length, y.Length)] : string.Empty;
     }
 
-    private static bool IsRepeatedSubstring(string sub, string original) {
-        for (int i = 0; i < original.Length; i += sub.Length) {
-            string cmpSub = original.Substring(i, sub.Length);    
+    static int GCD(int a, int b) => b == 0 ? a : GCD(b, a % b);
+
+    // public static string FindGDCString(string x, string y) {
+    //     if (x.Length == y.Length && x != y) {
+    //         return string.Empty;
+    //     }
+
+    //     // Make X a smaller string
+    //     if (x.Length < y.Length) {
+    //         (x, y) = (y, x);
+    //     }
+
+    //     // Start from the biggest
+    //     for (int i = x.Length; i > 0; i--) {
+    //         // Ignore non-common divisors
+    //         if (x.Length % i != 0 || y.Length % i != 0) {
+    //             continue;
+    //         }
+
+    //         // Get substring of X
+    //         string sub = x[..i];
+
+    //         // Check whether it is repeating in X and Y
+    //         if (IsRepeatedSubstring(sub, x) && IsRepeatedSubstring(sub, y)) {
+    //             return sub;
+    //         }
+    //     }
+
+    //     return string.Empty;
+    // }
+
+    // private static bool IsRepeatedSubstring(string sub, string original) {
+    //     for (int i = 0; i < original.Length; i += sub.Length) {
+    //         string cmpSub = original.Substring(i, sub.Length);    
             
-            if (sub != cmpSub) {
-                return false;
-            }
-        }
+    //         if (sub != cmpSub) {
+    //             return false;
+    //         }
+    //     }
 
-        return true;
-    }
+    //     return true;
+    // }
 }
