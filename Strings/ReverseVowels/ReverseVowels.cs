6bcaaf62e8a548684c51342adc35918c1be1e8a3
Vladimir <limtis2109@gmail.com>
2023-11-30 17:33:58 +0100

    Code formatting

--- Strings/ReverseVowels/ReverseVowels.cs
+++ Strings/ReverseVowels/ReverseVowels.cs
@@ -3,7 +3,8 @@ namespace LeetCodeChallenge;
 // 345. https://leetcode.com/problems/reverse-vowels-of-a-string/
 public class ReverseVowels
 {
-    private static readonly HashSet<char> vowels = new() { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+    private static readonly HashSet<char> vowels =
+        new() { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
 
     public static string Reverse(string s)
     {
@@ -12,21 +13,23 @@ public class ReverseVowels
         int p1 = 0;
         int p2 = s.Length - 1;
 
-        while (p1 < p2) 
+        while (p1 < p2)
         {
             // If not a vowel
-            if (!vowels.Contains(s[p1])) {
+            if (!vowels.Contains(s[p1]))
+            {
                 chars[p1] = s[p1];
                 p1++;
                 continue;
             }
 
             // Find the closest vowel for p2
-            while (p2 > p1 && !vowels.Contains(s[p2])) {
+            while (p2 > p1 && !vowels.Contains(s[p2]))
+            {
                 chars[p2] = s[p2];
                 p2--;
             }
-            
+
             // Swap vowels and move pointers
             (chars[p1], chars[p2]) = (s[p2], s[p1]);
             p2--;
@@ -34,8 +37,9 @@ public class ReverseVowels
         }
 
         // Cover the edge-case of missing the last char
-        if (chars[p1] == '\0') {
-            chars[p1] = s[p1];   
+        if (chars[p1] == '\0')
+        {
+            chars[p1] = s[p1];
         }
 
         return new string(chars);
